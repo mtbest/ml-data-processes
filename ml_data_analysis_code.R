e89b114c31bc871f1008d3986f4b41ad2f4ca6d0
mtbest <matt.motivatelab@gmail.com>
2022-10-27 11:18:34 -0400

    Setting up the analysis code sections

---
+++ ml_data_analysis_code.R
@@ -0,0 +1,185 @@
+#############################
+##
+## ML DATA ANALYSIS CODE
+##
+#############################
+
+#
+#
+# ANOVAs with post-hocs
+#
+#
+
+
+
+
+#
+#
+# Correlation matrix
+#
+#
+
+
+
+
+
+#
+#
+# Creating great tables and graphs
+#
+#
+
+
+
+#
+#
+# EFA
+#
+#
+
+
+
+
+#
+#
+# CFA
+#
+#
+
+
+
+
+#
+#
+# Path analysis and SEM
+#
+#
+
+
+
+
+#
+#
+# Reliability / Cronbach's alpha coefficients
+#
+#
+
+
+
+
+#
+#
+# Linear regression
+#
+#
+
+
+
+
+#
+#
+# Logistic regression
+#
+#
+
+
+
+
+#
+#
+# Tests of baseline equivalence
+#
+#
+
+
+
+
+#
+#
+# Multilevel models / HLMs
+#
+#
+
+
+
+
+#
+#
+# Response surface analysis
+#
+#
+
+
+
+
+#
+#
+# Cluster analysis
+#
+#
+
+
+
+
+#
+#
+# Multigroup analysis (testing indirect effects in a path model for different demographic groups)
+#
+#
+
+
+
+#
+#
+# Profile analysis
+#
+#
+
+
+
+
+#
+#
+# Power analysis
+#
+#
+
+
+
+#
+#
+# Bayesian predictive models
+#
+#
+
+
+
+
+#
+#
+# Mediation/moderation analysis
+#
+#
+
+
+
+
+#
+#
+# Propensity score matching
+#
+#
+
+
+
+
+#
+#
+# Creating a color scheme to match our ML branding and use in our graphs
+#
+#
+
+
+
+
+
