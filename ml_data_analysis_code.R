e8d9fb55c8bdd15d80df30ca30c28d721d2c269f
mtbest <matt.motivatelab@gmail.com>
2022-10-27 11:35:11 -0400

    ANOVA with post-hocs code plus correlation matrix

--- ml_data_analysis_code.R
+++ ml_data_analysis_code.R
@@ -10,6 +10,12 @@
 #
 #
 
+dv_gv_AOV <- aov(lm(dv ~ as.factor(gv), data = d)) # dv is the dependent variable
+TukeyHSD(dv_gv_AOV)                                # and gv is the group variable
+
+pander(summary(aov(dv ~ gv, data = d)))
+pander(summarySE(d, groupvar="gv", measurevar="dv",na.rm=TRUE),round=2)
+
 
 
 
@@ -19,7 +25,30 @@
 #
 #
 
+# Creating a function to create nice correlation matrices
 
+cortable <- function(x){ 
+  require(Hmisc) 
+  x <- as.matrix(x) 
+  R <- rcorr(x)$r 
+  p <- rcorr(x)$P 
+  ## define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
+  ## truncate the matrix that holds the correlations to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
+  ## build a new matrix that includes the correlations with their appropriate stars 
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
+  diag(Rnew) <- paste(diag(R), " ", sep="") 
+  rownames(Rnew) <- colnames(x) 
+  colnames(Rnew) <- paste(colnames(x), "", sep="") 
+  ## remove upper triangle
+  Rnew <- as.matrix(Rnew)
+  Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+  Rnew <- as.data.frame(Rnew) 
+  ## remove last column and return the matrix (which is now a data frame)
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  return(Rnew) 
+} 
 
 
 
